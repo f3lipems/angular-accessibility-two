92a1eaba5e560ce1e25c5650def305e02a5e4208
Felipe <felipe@visionar.io>
2023-08-07 17:33:30 -0300

    Add templateRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'a11y-p2';
-
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>
+  
+  public title = 'Accessibility Two';
   public firstName = 'Felipe';
+  public modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) { }
+
+  showModal() {
+    this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details'
+    })
+  }
 }
